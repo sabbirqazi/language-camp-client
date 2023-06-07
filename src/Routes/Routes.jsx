e673646ad197a8f1904629e5da12cbd04e74b5b0
Sabbir Ahmed <sabbirahmed.probd@gmail.com>
2023-06-07 15:58:31 +0600

    navbar added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
 
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Main></Main>
       },
 ])
