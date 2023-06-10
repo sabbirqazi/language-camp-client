bb8f0592426f5553384a534dc84c4136cce016d7
Sabbir Ahmed <sabbirahmed.probd@gmail.com>
2023-06-10 21:44:35 +0600

    approved and denied and feedback sent

--- src/pages/Dashboard/Admin/ManageClasses/ManageClasses.jsx
+++ src/pages/Dashboard/Admin/ManageClasses/ManageClasses.jsx
@@ -1,84 +1,144 @@
-import { useEffect, useState } from "react";
-
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import Swal from "sweetalert2";
 
 const ManageClasses = () => {
-    const [classes, setClasses] = useState([]);
 
-    const [feedbackModalOpen, setFeedbackModalOpen] = useState(false);
-    const [feedbackText, setFeedbackText] = useState('');
-    const [selectedClassId, setSelectedClassId] = useState('');
-    useEffect(() => {
-      fetch("http://localhost:5000/classes")
-        .then((res) => res.json())
-        .then((data) => {
-          const popularClasses = data.filter((item) => item.availableSeats > 0);
-          popularClasses.sort((a, b) => b.availableSeats - a.availableSeats);
-          setClasses(popularClasses);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
-    }, []);
 
-    const handleApprove = (classId) => {
-        // Handle approve logic
-        console.log(`Approve class ${classId}`);
-      };
-    
-      const handleDeny = (classId) => {
-      
-        // Handle deny logic
-        console.log(`Deny class ${classId}`);
-      };
-    
-      const handleSendFeedback = (classId) => {
-        setSelectedClassId(classId);
+  const [feedbackModalOpen, setFeedbackModalOpen] = useState(false);
+  const [feedbackText, setFeedbackText] = useState("");
+  const [selectedClassId, setSelectedClassId] = useState("");
+
+
+  const { data: classes = [], refetch } = useQuery(["classes"], async () => {
+    const res = await fetch("http://localhost:5000/classes");
+    return res.json();
+  });
+
+  const handleApprove = (classItem) => {
+    fetch(
+      `http://localhost:5000/classes/manageclasses/approve/${classItem._id}`,
+      {
+        method: "PATCH",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${classItem.name} is Approved!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+    console.log(`Approve class ${classItem}`);
+  };
+
+  const handleDeny = (classItem) => {
+    // Handle deny logic
+    fetch(`http://localhost:5000/classes/manageclasses/deny/${classItem._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${classItem.name} is Denied!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+    console.log(`Deny class ${classItem}`);
+  };
+
+  const handleSendFeedback = (classItem) => {
+    setSelectedClassId(classItem._id);
     setFeedbackModalOpen(true);
-        // Handle send feedback logic
-        console.log(`Send feedback for class ${classId}`);
-      };
-      const handleSubmitFeedback = () => {
-        // Send the feedback text to the instructor
-        console.log(`Send feedback for class ${selectedClassId}: ${feedbackText}`);
-    
-        // Close the feedback modal and reset the feedback text
-        setFeedbackModalOpen(false);
-        setFeedbackText('');
-      };
-    
-    return (
-        <div className="flex justify-center mt-8">
-        <div className="w-full">
-          <h2 className="text-2xl font-bold mb-4 text-center">Classes</h2>
-          <table className="table text-md table-zebra w-full p-4">
-            <thead>
-              <tr>
-                <th className=" text-lg py-2 px-4 border-b">£</th>
-                <th className=" text-lg py-2 px-4 border-b">Name</th>
-                <th className=" text-lg py-2 px-4 border-b">Image</th>
-                <th className=" text-lg py-2 px-4 border-b">Email</th>
-                <th className=" text-lg py-2 px-4 border-b">Seats</th>
-                <th className=" text-lg py-2 px-4 border-b">Price</th>
-                <th className=" text-lg py-2 px-4 border-b">Status</th>
-                <th className=" text-lg py-2 px-4 border-b">Actions</th>
-             
-              </tr>
-            </thead>
-            <tbody>
-              {classes?.map((classItem, index) => (
-                <tr key={classItem._id}>
-                  <td className="py-2 px-4 border-b">{index+1}</td>
-                  <td className="py-2 px-4 border-b">{classItem.name}</td>
-                  <td className="py-2 px-4 border-b"><img src={classItem.image} alt={classItem.name} className="w-16 h-16 object-cover rounded-md" /></td>
-                  <td className="py-2 px-4 border-b">{classItem.email}</td>
-                  <td className="py-2 px-4 border-b">{classItem.seats}</td>
-                  <td className="py-2 px-4 border-b">{classItem.price}</td>
-                  <td className="py-2 px-4 border-b">{classItem.status}</td>
-                  <td className="py-2 px-4 border-b">
+
+    console.log(`Send feedback for class ${classItem}`);
+  };
+  const handleSubmitFeedback = () => {
+    const classesItem = classes.find((item) => item._id === selectedClassId);
+    fetch(`http://localhost:5000/classes/manageclasses/deny/${classesItem._id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({feedbackText}),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `feedback send for ${classesItem.name} !`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+    // Send the feedback text to the instructor
+    console.log(`Send feedback for class ${selectedClassId}: ${feedbackText}`);
+
+    // Close the feedback modal and reset the feedback text
+    setFeedbackModalOpen(false);
+    setFeedbackText("");
+  };
+
+  return (
+    <div className="flex justify-center mt-8">
+      <div className="w-full">
+        <h2 className="text-2xl font-bold mb-4 text-center">Classes</h2>
+        <table className="table text-md table-zebra w-full p-4">
+          <thead>
+            <tr>
+              <th className=" text-lg py-2 px-4 border-b">£</th>
+              <th className=" text-lg py-2 px-4 border-b">Name</th>
+              <th className=" text-lg py-2 px-4 border-b">Image</th>
+              <th className=" text-lg py-2 px-4 border-b">Email</th>
+              <th className=" text-lg py-2 px-4 border-b">Seats</th>
+              <th className=" text-lg py-2 px-4 border-b">Price</th>
+              <th className=" text-lg py-2 px-4 border-b">Status</th>
+              <th className=" text-lg py-2 px-4 border-b">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+          
+            {classes?.map((classItem, index) => (
+              <tr key={classItem._id}>
+                <td className="py-2 px-4 border-b">{index + 1}</td>
+                <td className="py-2 px-4 border-b">{classItem.name}</td>
+                <td className="py-2 px-4 border-b">
+                  <img
+                    src={classItem.image}
+                    alt={classItem.name}
+                    className="w-16 h-16 object-cover rounded-md"
+                  />
+                </td>
+                <td className="py-2 px-4 border-b">{classItem.email}</td>
+                <td className="py-2 px-4 border-b">{classItem.seats}</td>
+                <td className="py-2 px-4 border-b">{classItem.price}</td>
+                <td className="py-2 px-4 border-b">{classItem.status}</td>
+                <td className="py-2 px-4 border-b">
                   <button
                     className="bg-green-500 text-white px-4 py-2 rounded"
-                    onClick={() => handleApprove(classItem.id)}
-                    disabled={classItem.status !== 'pending'}
+                    onClick={() => handleApprove(classItem)}
+                    disabled={classItem.status !== "pending"}
                   >
                     Approve
                   </button>
@@ -86,8 +146,8 @@ const ManageClasses = () => {
                 <td className="py-2 px-4 border-b">
                   <button
                     className="bg-red-500 text-white px-4 py-2 rounded"
-                    onClick={() => handleDeny(classItem.id)}
-                    disabled={classItem.status !== 'pending'}
+                    onClick={() => handleDeny(classItem)}
+                    disabled={classItem.status !== "pending"}
                   >
                     Deny
                   </button>
@@ -95,18 +155,18 @@ const ManageClasses = () => {
                 <td className="py-2 px-4 border-b">
                   <button
                     className="bg-blue-500 text-white px-4 py-2 rounded"
-                    onClick={() => handleSendFeedback(classItem.id)}
-                    disabled={classItem.status !== 'pending'}
+                    onClick={() => handleSendFeedback(classItem)}
+                    disabled={classItem.status !== "denied"}
                   >
                     Send Feedback
                   </button>
                 </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        {feedbackModalOpen && (
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {feedbackModalOpen && (
         <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
           <div className="bg-white rounded-lg p-6 max-w-sm">
             <h2 className="text-xl font-bold mb-4">Send Feedback</h2>
@@ -117,18 +177,24 @@ const ManageClasses = () => {
               onChange={(e) => setFeedbackText(e.target.value)}
             ></textarea>
             <div className="flex justify-end">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded mr-2" onClick={handleSubmitFeedback}>
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
+                onClick={handleSubmitFeedback}
+              >
                 Send
               </button>
-              <button className="bg-gray-300 text-gray-700 px-4 py-2 rounded" onClick={() => setFeedbackModalOpen(false)}>
+              <button
+                className="bg-gray-300 text-gray-700 px-4 py-2 rounded"
+                onClick={() => setFeedbackModalOpen(false)}
+              >
                 Cancel
               </button>
             </div>
           </div>
         </div>
       )}
-      </div>
-    );
+    </div>
+  );
 };
 
 export default ManageClasses;
